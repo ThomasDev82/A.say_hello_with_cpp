21fc421b5b4544974337db217b533def075e2b55
ThomasDev82 <lancevancy7@gmail.com>
2022-08-11 02:04:39 +0200

    Problem - A

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // this program is written by Thomas
+
+    string name;
+
+    cin >> name;
+
+
+    cout << "Hello, " + name << endl;
+
+
+    return 0;
+}
